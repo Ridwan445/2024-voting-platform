7b4708fed665d97736d246c6d99f95001eba7e91
Ridwan <sanniridwan070@gmail.com>
2024-07-23 16:58:34 +0100

    commit

--- app.js
+++ app.js
@@ -5,29 +5,31 @@ const session = require('express-session');
 const multer = require('multer');
 const fs = require('fs');
 
+
+
 const allowedFlatNumbers = [
-  'BLK A 1A', 'BLK A 1B', 'BLK A 2A', 'BLK A 2B', 'BLK A 3A', 'BLK A 3B', 'BLK B 1B', 
-  'BLK B 2A', 'BLK B 2B', 'BLK B 3A', 'BLK B 3B', 'BLK B 4A', 'BLK B 4B', 'BLK C 1A', 
-  'BLK C 1B', 'BLK C 2A', 'BLK C 2B', 'BLK C 3A', 'BLK C 3B', 'BLK C 4A', 'BLK C 4B', 
-  'BLK D 1A', 'BLK D 1B', 'BLK D 2A', 'BLK D 2B', 'BLK D 3A', 'BLK D 3B', 'BLK D 4A', 
-  'BLK D 4B', 'BLK E 1A', 'BLK E 1B', 'BLK E 2A', 'BLK E 2B', 'BLK E 3A', 'BLK E 3B', 
-  'BLK E 4A', 'BLK E 4B', 'BLK F 1A', 'BLK F 1B', 'BLK F 2A', 'BLK F 2B', 'BLK F 3A', 
-  'BLK F 3B', 'BLK F 4A', 'BLK F 4B', 'BLK G 1A', 'BLK G 1B', 'BLK G 2A', 'BLK G 2B', 
-  'BLK G 3A', 'BLK G 3B', 'BLK H 1A', 'BLK H 1B', 'BLK H 2A', 'BLK H 2B', 'BLK H 3A', 
-  'BLK H 3B', 'BLK I 1A', 'BLK I 1B', 'BLK I 2A', 'BLK I 2B', 'BLK I 3A', 'BLK I 3B', 
-  'BLK J 1A', 'BLK J 1B', 'BLK J 2A', 'BLK J 2B', 'BLK J 3A', 'BLK J 3B', 'BLK K 1A', 
-  'BLK K 1B', 'BLK K 2A', 'BLK K 2B', 'BLK K 3A', 'BLK K 3B', 'BLK L 1A', 'BLK L 1B', 
-  'BLK L 2A', 'BLK L 2B', 'BLK L 3A', 'BLK L 3B', 'BLK M 1A', 'BLK M 1B', 'BLK M 2A', 
-  'BLK M 2B', 'BLK M 3A', 'BLK M 3B', 'BLK M 4A', 'BLK M 4B', 'BLK M 5A', 'BLK M 5B', 
-  'BLK N 1A', 'BLK N 1B', 'BLK N 2A', 'BLK N 2B', 'BLK N 3A', 'BLK N 3B', 'BLK N 4A', 
-  'BLK N 4B', 'BLK N 5A', 'BLK N 5B', 'BLK O1', 'BLK O2', 'BLK O3', 'BLK O4', 'BLK O5', 
-  'BLK O6', 'BLK O7', 'BLK O8', 'BLK O9', 'BLK O10', 'BLK O11', 'BLK O12', 'BLK O13', 
-  'BLK O14', 'BLK O15', 'BLK O16', 'BLK P1', 'BLK P2', 'BLK P3', 'BLK P4', 'BLK P5', 
-  'BLK P6', 'BLK P7', 'BLK P8', 'BLK P9', 'BLK P10', 'BLK P11', 'BLK P12', 'BLK P13', 
-  'BLK P14', 'BLK P15', 'BLK P16', 'BLK Q1', 'BLK Q2', 'BLK Q3', 'BLK Q4', 'BLK Q5', 
-  'BLK Q6', 'BLK Q7', 'BLK Q8', 'BLK Q9', 'BLK Q10', 'BLK Q11', 'BLK Q12', 'BLK Q13', 
-  'BLK Q14', 'BLK Q15', 'BLK Q16', 'BLK R 1A', 'BLK R 1B', 'BLK R 2A', 'BLK R 2B', 
-  'BLK R 3A', 'BLK R 3B', 'BLK R 4A', 'BLK R 4B', 'BLK R 5A', 'BLK R 5B'
+  'A1A', 'A1B', 'A2A', 'A2B', 'A3A', 'A3B', 'B1B', 
+  'B2A', 'B2B', 'B3A', 'B3B', 'B4A', 'B4B', 'C1A', 
+  'C1B', 'C2A', 'C2B', 'C3A', 'C3B', 'C4A', 'C4B', 
+  'D1A', 'D1B', 'D2A', 'D2B', 'D3A', 'D3B', 'D4A', 
+  'D4B', 'E1A', 'E1B', 'E2A', 'E2B', 'E3A', 'E3B', 
+  'E4A', 'E4B', 'F1A', 'F1B', 'F2A', 'F2B', 'F3A', 
+  'F3B', 'F4A', 'F4B', 'G1A', 'G1B', 'G2A', 'G2B', 
+  'G3A', 'G3B', 'H1A', 'H1B', 'H2A', 'H2B', 'H3A', 
+  'H3B', 'I1A', 'I1B', 'I2A', 'I2B', 'I3A', 'I3B', 
+  'J1A', 'J1B', 'J2A', 'J2B', 'J3A', 'J3B', 'K 1A', 
+  'K1B', 'K2A', 'K2B', 'K3A', 'K3B', 'L1A', 'L1B', 
+  'L2A', 'L2B', 'L3A', 'L3B', 'M1A', 'M1B', 'M2A', 
+  'M2B', 'M3A', 'M3B', 'M4A', 'M4B', 'M5A', 'M5B', 
+  'N1A', 'N1B', 'N2A', 'N2B', 'N3A', 'N3B', 'N4A', 
+  'N4B', 'N5A', 'N5B', 'O1', 'O2', 'O3', 'O4', 'O5', 
+  'O6', 'O7', 'O8', 'O9', 'O10', 'O11', 'O12', 'O13', 
+  'O14', 'O15', 'O16', 'P1', 'P2', 'P3', 'P4', 'P5', 
+  'P6', 'P7', 'P8', 'P9', 'P10', 'P11', 'P12', 'P13', 
+  'P14', 'P15', 'P16', 'Q1', 'Q2', 'Q3', 'Q4', 'Q5', 
+  'Q6', 'Q7', 'Q8', 'Q9', 'Q10', 'Q11', 'Q12', 'Q13', 
+  'Q14', 'Q15', 'Q16', 'R1A', 'R1B', 'R2A', 'R2B', 
+  'R3A', 'R3B', 'R4A', 'R4B', 'R5A', 'R5B'
 ];
 
 const app = express();
@@ -98,11 +100,12 @@ app.get('/candidates', (req, res) => {
   }
 });
 
-// Submit individual vote
 app.post('/vote', (req, res) => {
-  const { candidateIndex, vote } = req.body;
+  const { candidateName, vote } = req.body;
   const flatNumber = req.session.flatNumber;
-  const candidate = candidates[candidateIndex]?.name;
+  const email = req.session.email; 
+
+  console.log('Vote Received:', { candidateName, vote, flatNumber });
 
   if (!flatNumber) {
     return res.redirect('/');
@@ -112,17 +115,20 @@ app.post('/vote', (req, res) => {
 
   if (votes[flatNumber] < 2) {
     votes[flatNumber]++;
-    if (candidate) {
-      if (!voteCount[candidate]) {
-        voteCount[candidate] = { yes: 0, no: 0 };
+    if (candidateName) {
+      if (!voteCount[candidateName]) {
+        voteCount[candidateName] = { yes: 0, no: 0 };
       }
       if (vote === 'yes') {
-        voteCount[candidate].yes++;
+        voteCount[candidateName].yes++;
       } else if (vote === 'no') {
-        voteCount[candidate].no++;
+        voteCount[candidateName].no++;
       }
     }
-    res.redirect(`/thankyou`);
+
+    console.log('Updated Vote Count:', voteCount);
+    saveData();
+    res.redirect('/thankyou');
   } else {
     res.render('candidates', {
       candidates,
@@ -133,16 +139,29 @@ app.post('/vote', (req, res) => {
   }
 });
 
-app.get('/thankyou', (req, res) => {
-  res.render('thankyou', {
-    message: 'Your vote has been submitted.',
-    alertClass: 'alert-success'
-  });
-});
+const loadData = () => {
+  const dataPath = path.join(__dirname, 'voteData.json');
+  if (fs.existsSync(dataPath)) {
+    const rawData = fs.readFileSync(dataPath, 'utf-8');
+    voteCount = JSON.parse(rawData);
+  }
+};
+
+const saveData = () => {
+  const dataPath = path.join(__dirname, 'voteData.json');
+  fs.writeFileSync(dataPath, JSON.stringify(voteCount, null, 2), 'utf-8');
+};
+
+app.get("/thankyou", (req, res) => {
+  res.render("thankyou")
+})
+
+
 
 app.get('/admin-login', (req, res) => {
   res.render('admin-login', { message: null, alertClass: null });
 });
+
 
 app.post('/admin-login', (req, res) => {
   const { password } = req.body;
@@ -167,6 +186,7 @@ const ensureAdmin = (req, res, next) => {
 
 app.get('/admin', ensureAdmin, (req, res) => {
   console.log('Vote Count:', voteCount); 
+  loadData();
   res.render('admin', { candidates, voteCount });
 });
 
